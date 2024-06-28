ddda1c6f495f2ab975dc9c003e889e5ce740e0af
crea-tivecoder <nyawirawaweru.com>
2024-06-28 19:38:10 +0300

    adds arrow functions lab

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression called divide
+const divide = function(divide) {
+    return 2000/100;
+    console.log(divide(2000/100));
+  }
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num
+  };
+  console.log(square(2));
+  
+  // Arrow function called add
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+    add(4,3);
+
+  
